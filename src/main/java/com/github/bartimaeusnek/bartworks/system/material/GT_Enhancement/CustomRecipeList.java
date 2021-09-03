231f422cb6a64f3e201cffce192f6ef424b68c8e
observeroftime02 <observeroftime@shizuki.ca>
2021-09-02 18:33:04 -0700

    The 10:1 nature of osmium production is no good. let's make it 3:1

--- src/main/java/com/github/bartimaeusnek/bartworks/system/material/GT_Enhancement/CustomRecipeList.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/system/material/GT_Enhancement/CustomRecipeList.java
@@ -52,7 +52,7 @@ public class CustomRecipeList {
             GT_Values.RA.addChemicalRecipe(GT_OreDictUnificator.get(dust, Materials.Potassium, 2L), GT_OreDictUnificator.get(dust, Materials.Sulfur, 2L), Materials.Oxygen.getGas(7000L), PotassiumDisulfate.getMolten(11000), null, 42, 90);
 
             // Better DT Tower Recipes for PGM Processing (bigger recipes I/O wise, reduced time)
-            GT_Values.RA.addDistillationTowerRecipe(AcidicOsmiumSolution.getFluidOrGas(10000), new FluidStack[]{OsmiumSolution.getFluidOrGas(1000), Materials.Water.getFluid(9000)}, null, 375, Voltage.IV.getVoltage());
+            GT_Values.RA.addDistillationTowerRecipe(AcidicOsmiumSolution.getFluidOrGas(10000), new FluidStack[]{OsmiumSolution.getFluidOrGas(7000), Materials.Water.getFluid(3000)}, null, 375, Voltage.IV.getVoltage());
             GT_Values.RA.addDistillationTowerRecipe(HotRutheniumTetroxideSollution.getFluidOrGas(9000), new FluidStack[]{Materials.Water.getFluid(1800), RutheniumTetroxide.getFluidOrGas(7200)}, Materials.Salt.getDust(6), 375, Voltage.HV.getVoltage());
 
 
